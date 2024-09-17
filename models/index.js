906bb809088a6fef240cf2e9da32912564b2e945
David John <84499347+DavidJohndps@users.noreply.github.com>
2024-09-17 22:34:09 +0800

    database connection reflect env

--- models/index.js
+++ models/index.js
@@ -5,8 +5,16 @@ const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const env = process.env.NODE_ENV || 'development';
+const dbConfig = {
+  username:process.env.DB_USERNAME,
+  password:process.env.DB_PASSWORD,
+  database:process.env.DB_NAME,
+  host:process.env.DB_HOST,
+  dialect:process.env.DB_DIALECT,
+  port:process.env.DB_PORT
+}
+const config = dbConfig
 const db = {};
 
 let sequelize;
