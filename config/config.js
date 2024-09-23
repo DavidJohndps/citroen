bf0bbfb4bbd444e951a287274ce2b049f17a8f44
David John <84499347+DavidJohndps@users.noreply.github.com>
2024-09-23 22:40:53 +0700

    added default config value

--- config/config.js
+++ config/config.js
@@ -1,11 +1,11 @@
 const config = {
   "development": {
-    "username": process.env.DB_DEV_USERNAME,
-    "password": process.env.DB_DEV_PASSWORD,
-    "database": process.env.DB_DEV_NAME,
-    "host": process.env.DB_DEV_HOST,
-    "dialect": process.env.DB_DEV_DIALECT,
-    "port": process.env.D_DEVB_PORT
+    "username": process.env.DB_DEV_USERNAME || "root",
+    "password": process.env.DB_DEV_PASSWORD || "",
+    "database": process.env.DB_DEV_NAME || "citroen",
+    "host": process.env.DB_DEV_HOST || "127.0.0.1",
+    "dialect": process.env.DB_DEV_DIALECT || "mysql",
+    "port": process.env.D_DEVB_PORT || "3306"
   },
   "test": {
     "username": "root",
