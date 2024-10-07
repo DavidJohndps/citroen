f48f94e99db23540f6dcfac0ff7a474b05e6123e
David John <84499347+DavidJohndps@users.noreply.github.com>
2024-10-07 16:32:47 +0700

    updated booking email

--- routes/booking/index.js
+++ routes/booking/index.js
@@ -7,13 +7,14 @@ const { Op } = require('sequelize');
 const path = require('path');
 const ejs = require('ejs');
 const htmlPdf = require('html-pdf');
+const moment = require('moment');
 
-
+moment.locale('id')
 router.post('/', async (req, res) => {
     try {
         
         const {body: data} = req
-        const {type, carData: carId, selectedColor: color, selectedAccessory: accessory, KTPName: name, PhoneNumber: phoneNumber, email, provincies: provinceId, city: cityId, address} = data
+        const {type, carData: carId, selectedColor: color, selectedAccessory: accessory, KTPName: name, PhoneNumber: phoneNumber, email, provincies: provinceId, city: cityId, fullAddress: address} = data
         
         const car = await Car.findOne({
             where: {
@@ -35,8 +36,8 @@ router.post('/', async (req, res) => {
         let city
         let attachment
         let subject
-        // const bcc = 'noreply@citroen.indomobil.co.id, care@citroen.indomobil.co.id, ferdinan.hendra@citroen.indomobil.co.id, galih.pamungkas@citroen.indomobil.co.id, heri.kurniawan@citroen.indomobil.co.id, ulung.windi@citroen.indomobil.co.id'
-        const bcc = 'fauzanamrian12@gmail.com, daffa.firdaus13@gmail.com'
+        const bcc = 'noreply@citroen.indomobil.co.id, care@citroen.indomobil.co.id, ferdinan.hendra@citroen.indomobil.co.id, galih.pamungkas@citroen.indomobil.co.id, heri.kurniawan@citroen.indomobil.co.id, ulung.windi@citroen.indomobil.co.id'
+        // const bcc = 'fauzanamrian12@gmail.com, daffa.firdaus13@gmail.com'
         if (type === 'Get Quotation') {
             dealer = await Dealer.findOne({
                 where: {
@@ -70,11 +71,7 @@ router.post('/', async (req, res) => {
                 const accessoryPrice = selectedAccessory.prices.find(x => x.provinceId === provinceId)
                 const pdfPayload = {
                     status: 'Menunggu Konfirmasi',
-                    quotationDate: new Date().toLocaleDateString('id-ID', {
-                        day: '2-digit',
-                        month: 'long', // You can use '2-digit' for numeric month or 'long' for full month name
-                        year: 'numeric'
-                    }),
+                    quotationDate: moment().add(3, 'M').format('dddd, Do MMMM YYYY'),
                     requester: {
                         name,
                         phone: phoneNumber,
@@ -85,20 +82,19 @@ router.post('/', async (req, res) => {
                     },
                     orderQuantity: 1,
                     vehicle: {
-                        name: car.name,
+                        name: car.name.replace('|', ''),
                         color: carGallery.name,
                         price: carGalleryPrice.price,
                         option: selectedAccessory.name,
                         optionDesc: selectedAccessory.desc,
                         optionPrice: accessoryPrice.price,
                         total: carGalleryPrice.price + accessoryPrice.price,
-                        image: `https://api.citroen-info.id/${car.img.split('public/')[1]}`
+                        image: `https://api.citroen-info.id/${carGallery.path.split('public/')[1]}`
                     }
                 }
                 text = 
                 `Hallo, ${name}. \n\n We're excited to have you get started with Citroën! \n Pada email ini kami lampirkan e-Quotation untuk mobil Citroën Anda. Silahkan tunjukkan kode QR atau Nomor Seri di bawah ini kepada salah satu staf Citroën di sekitar Anda atau Anda dapat mendatangi Dealer resmi Citroën untuk melanjutkan proses pemesanan Anda! \n\n Jika Anda memiliki pertanyaan, biarkan kami membantu Anda! \n Hubungi kami melalui WhatsApp(${dealer.pic})`
             
-                console.log(pdfPayload.vehicle.image)
                 // Render the EJS template to HTML for the PDF
                 const html = await ejs.renderFile(path.resolve(__dirname, '../../', 'assets/templates/quotation.ejs'), pdfPayload);
 
@@ -112,8 +108,6 @@ router.post('/', async (req, res) => {
                         bottom: '20px',
                         left: '20px'
                     },
-                    // width: '800px', // Forces content width to match CSS layout width.
-                    // height: 'auto'  // Allows the height to adapt based on content.
                 };
                 htmlPdf.create(html, options).toBuffer(async (err, pdfBuffer) => {
                     if (err) {
@@ -121,7 +115,7 @@ router.post('/', async (req, res) => {
                     }
 
                     attachment = {
-                        filename: 'Test.pdf',
+                        filename: `E-Quotation - ${car.name.replace('|','')} | ${moment().format('D-M-YYYY')}.pdf`,
                         content: pdfBuffer
                     }
 
